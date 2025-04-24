d5b030c9db36ee5d63cbd4b3894a7e98867ea0c6
Braaacke <65670146+Braaacke@users.noreply.github.com>
2025-04-25 00:00:54 +0200

    Update Bearbeitungstool.py

--- Bearbeitungstool.py
+++ Bearbeitungstool.py
@@ -283,5 +283,3 @@ if st.button("Zuordnung exportieren"):
         file_name="routen_zuweisung_aktualisiert.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
-
-m.to_streamlit(height=700)(height=700)
