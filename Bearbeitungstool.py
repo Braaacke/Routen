035d1b35dde63c5343715c74e99b608513578870
Braaacke <65670146+Braaacke@users.noreply.github.com>
2025-05-09 15:59:20 +0200

    Update Bearbeitungstool.py

--- Bearbeitungstool.py
+++ Bearbeitungstool.py
@@ -19,12 +19,8 @@ from math import radians, sin, cos, sqrt, asin
 def haversine(lon1, lat1, lon2, lat2):
     dlon = radians(lon2 - lon1)
     dlat = radians(lat2 - lat1)
-    a = sin(dlat/2)**2 + cos(radians(lat1))*cos(radians(lat2))*sin(dlon/2)**2
-    return 2*6371000*asin(sqrt(a))(lon1, lat1, lon2, lat2):
-    dlon = radians(lon2 - lon1)
-    dlat = radians(lat2 - lat1)
-    a = sin(dlat/2)**2 + cos(radians(lat1))*cos(radians(lat2))*sin(dlon/2)**2
-    return 2*6371000*asin(sqrt(a))
+    a = sin(dlat/2)**2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon/2)**2
+    return 2 * 6371000 * asin(sqrt(a))
 
 # Caching graph
 @st.cache_data
